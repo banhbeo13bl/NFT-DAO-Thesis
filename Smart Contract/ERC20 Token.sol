e986581e71d32a54dcb42b8f92288fa12a99c31e
banhbeo13bl <59357887+banhbeo13bl@users.noreply.github.com>
2023-05-23 23:00:08 +0700

    Commit 2

--- Smart Contract/ERC20 Token.sol
+++ Smart Contract/ERC20 Token.sol
@@ -1,11 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract InvestmentDAO is ERC20, ERC20Burnable {
-    constructor() ERC20("Investment DAO", "ID") {
-        _mint(msg.sender, 10000000 * 10 ** decimals());
-    }
+contract NFTDAO is ERC20, ERC20Burnable, Ownable {
+
+  mapping(address => bool) controllers;
+  
+  constructor() ERC20("NFT DAO", "NFTD") { }
+
+  function mint(address to, uint256 amount) external {
+    require(controllers[msg.sender], "Only controllers can mint");
+    _mint(to, amount);
+  }
+
+  function burnFrom(address account, uint256 amount) public override {
+      if (controllers[msg.sender]) {
+          _burn(account, amount);
+      }
+      else {
+          super.burnFrom(account, amount);
+      }
+  }
+
+  function addController(address controller) external onlyOwner {
+    controllers[controller] = true;
+  }
+
+  function removeController(address controller) external onlyOwner {
+    controllers[controller] = false;
+  }
 }
